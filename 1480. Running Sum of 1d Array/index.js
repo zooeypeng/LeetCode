d56d8da610749d69760c1b57dec1d72bd0582f1b
zooeypeng <zooey.peng@gmail.com>
2023-08-02 13:45:05 -0700

    add solution for 1480. Running Sum of 1d Array

--- 1480. Running Sum of 1d Array/index.js
+++ 1480. Running Sum of 1d Array/index.js
@@ -8,3 +8,25 @@ var runningSum = function(nums) {
     let n = 0
     return nums.map(num => n += num)
 };
+
+// 下面這個是我原本寫的，發現 runtime 還比較快，可能是因為用的是 forEach
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let sumArr = [];
+    let n = 0
+
+    nums.forEach((num, i) => {
+        if (i === 0) {
+            n = num
+        } else {
+            n += nums[i]
+        }
+        sumArr.push(n)
+    })
+
+    return sumArr
+};
