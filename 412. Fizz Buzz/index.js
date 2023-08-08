df19d5dfeb02344ce43d7206e536c166ac49e704
zooeypeng <zooey.peng@gmail.com>
2023-08-07 17:55:55 -0700

    add solution for 412. Fizz Buzz

---
+++ 412. Fizz Buzz/index.js
@@ -0,0 +1,21 @@
+// Problem: https://leetcode.com/problems/fizz-buzz/
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+  let ans = []
+
+  for (let i = 1; i <= n; i++) {
+      let string = ''
+
+      if (i % 3 === 0) string += 'Fizz'
+      if (i % 5 === 0) string += 'Buzz'
+
+      if (string === '') string += i
+      
+      ans.push(string)
+  }
+  return ans
+};
